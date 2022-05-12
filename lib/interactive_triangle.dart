59bf4582c2f99c9e6dac41d2c9012fd22b85f906
Erik Näslund <contact@eriknaslund.com>
2022-05-12 20:49:23 +0200

    Refactor circle to object

--- lib/interactive_triangle.dart
+++ lib/interactive_triangle.dart
@@ -9,27 +9,32 @@ class MovablePoint extends StatefulWidget {
 
 enum Selected { none, c1, c2, c3 }
 
+class Circle {
+  double x;
+  double y;
+  double radius;
+  Circle({required this.x, required this.y, this.radius = 10});
+}
+
 class _MovablePointState extends State<MovablePoint> {
-  double xPos = 100.0;
-  double yPos = 100.0;
-  double xPos2 = 150.0;
-  double yPos2 = 200.0;
-  double xPos3 = 200.0;
-  double yPos3 = 100.0;
+  Circle c1 = Circle(x: 100.0, y: 100.0);
+  Circle c2 = Circle(x: 150.0, y: 200.0);
+  Circle c3 = Circle(x: 200.0, y: 100.0);
+
   Selected _selected = Selected.none;
   double radius = 10;
   bool _dragging = false;
 
   bool _insideCircle(double x, double y) {
-    if (x >= xPos - radius && x <= xPos + radius && y >= yPos - radius && y <= yPos + radius) {
+    if (x >= c1.x - c1.radius && x <= c1.x + c1.radius && y >= c1.y - c1.radius && c1.y <= c1.y + c1.radius) {
       _selected = Selected.c1;
       return true;
     }
-    if (x >= xPos2 - radius && x <= xPos2 + radius && y >= yPos2 - radius && y <= yPos2 + radius) {
+    if (x >= c2.x - c2.radius && x <= c2.x + c2.radius && y >= c2.y - c2.radius && c2.y <= c2.y + c1.radius) {
       _selected = Selected.c2;
       return true;
     }
-    if (x >= xPos3 - radius && x <= xPos3 + radius && y >= yPos3 - radius && y <= yPos3 + radius) {
+    if (x >= c3.x - c3.radius && x <= c3.x + c3.radius && y >= c3.y - c3.radius && c3.y <= c3.y + c1.radius) {
       _selected = Selected.c3;
       return true;
     } else {
@@ -52,16 +57,16 @@ class _MovablePointState extends State<MovablePoint> {
           setState(() {
             switch (_selected) {
               case Selected.c1:
-                xPos += details.delta.dx;
-                yPos += details.delta.dy;
+                c1.x += details.delta.dx;
+                c1.y += details.delta.dy;
                 break;
               case Selected.c2:
-                xPos2 += details.delta.dx;
-                yPos2 += details.delta.dy;
+                c2.x += details.delta.dx;
+                c2.y += details.delta.dy;
                 break;
               case Selected.c3:
-                xPos3 += details.delta.dx;
-                yPos3 += details.delta.dy;
+                c3.x += details.delta.dx;
+                c3.y += details.delta.dy;
                 break;
               default:
                 break;
@@ -72,27 +77,27 @@ class _MovablePointState extends State<MovablePoint> {
       child: Stack(
         children: [
           CustomPaint(
-            painter: LinePainter(startPos: Offset(xPos, yPos), endPos: Offset(xPos2, yPos2)),
+            painter: LinePainter(startPos: Offset(c1.x, c1.y), endPos: Offset(c2.x, c2.y)),
             child: Container(),
           ),
           CustomPaint(
-            painter: LinePainter(startPos: Offset(xPos3, yPos3), endPos: Offset(xPos, yPos)),
+            painter: LinePainter(startPos: Offset(c3.x, c3.y), endPos: Offset(c1.x, c1.y)),
             child: Container(),
           ),
           CustomPaint(
-            painter: LinePainter(startPos: Offset(xPos2, yPos2), endPos: Offset(xPos3, yPos3)),
+            painter: LinePainter(startPos: Offset(c2.x, c2.y), endPos: Offset(c3.x, c3.y)),
             child: Container(),
           ),
           CustomPaint(
-            painter: PointPainter(cPos: Offset(xPos, yPos), radius: radius),
+            painter: PointPainter(cPos: Offset(c1.x, c1.y), radius: radius),
             child: Container(),
           ),
           CustomPaint(
-            painter: PointPainter(cPos: Offset(xPos2, yPos2), radius: radius),
+            painter: PointPainter(cPos: Offset(c2.x, c2.y), radius: radius),
             child: Container(),
           ),
           CustomPaint(
-            painter: PointPainter(cPos: Offset(xPos3, yPos3), radius: radius),
+            painter: PointPainter(cPos: Offset(c3.x, c3.y), radius: radius),
             child: Container(),
           ),
         ],
